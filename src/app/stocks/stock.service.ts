028516060c6648112444b6bf1766c66d613e6cfb
Fu, Biye <biye.fu@outlook.com>
2019-08-18 16:45:17 +0800

    update stocks components

--- src/app/stocks/stock.service.ts
+++ src/app/stocks/stock.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Stock } from './model/stock';
+import { Stock } from './stock';
 
 @Injectable({
   providedIn: 'root'
--- src/app/stocks/stocks.component.ts
+++ src/app/stocks/stocks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Stock } from '../model/stock';
+import { Stock } from './stock';
+import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-stocks',
@@ -12,11 +13,19 @@ export class StocksComponent implements OnInit {
     symbol: 'sh600058',
     code: '600058',
     name: '五矿发展'
-  }
+  };
+  stocks: Stock[];
 
-  constructor() { }
+  constructor(private stockService: StockService) { }
 
   ngOnInit() {
+    this.getStocks();
+  }
+
+  getStocks(): void{
+    this.stockService.getStocks().subscribe(stocks => {
+      this.stocks = stocks;  
+    })
   }
 
 }
